9d649e6173f5960a36e308ff7314132bf90f545c
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-07-24 10:51:09 +0530

    navbar routs aded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Doctors from './pages/Doctors' 
-import Login from './pages/login'
-import Login from './pages/About'
-import Login from './pages/Contact'
+import Doctors from './pages/Doctores' 
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import MyAppointments from './pages/MyAppointments'
 import Myprofile from './pages/Myprofile'
 import Appointments from './pages/Appointments'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
       <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/docters' element={<Doctors />} />
